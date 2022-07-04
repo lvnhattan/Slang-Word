9f4222f856a60af70443c67a698e14772de03552
Nhat Tan <staan0906@gmail.com>
2022-07-04 15:33:43 +0700

    add Slang.txt and newslang.txt

--- Source/Main.java
+++ Source/Main.java
@@ -1,2 +1,148 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
+
+public class Main {
+    public static HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+    public static ArrayList<String> history = new ArrayList<String>();
+    public static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        ReadFile("newslang.txt");
+        if (map.isEmpty()) {
+            ReadFile("slang.txt");
+        }
+        Menu();
+    }
+
+    public static void Menu() {
+
+
+        System.out.println("======================MENU======================");
+        System.out.println("1. Search by Slang Word");
+        System.out.println("2. Search by Definition");
+        System.out.println("3. Show History");
+        System.out.println("4. Add a Slang Word");
+        System.out.println("5. Edit a Slang Word");
+        System.out.println("6. Delete a Slang Word");
+        System.out.println("7. Reset");
+        System.out.println("8. Get Random 1 Slang Word");
+        System.out.println("9. Find Definition by Slang word(Mini Game 1)");
+        System.out.println("10. Find Slang Word by Definition(Mini Game 2)");
+        System.out.println("11. Exit");
+        System.out.println("================================================");
+        System.out.print("Press Your choice is: ");
+        int choice = 0;
+        try {
+            choice = sc.nextInt();
+        } catch (Exception ex) {
+            choice = 0;
+            System.out.println("Your Value is Wrong. Error: " + ex);
+        }
+        String ignore = sc.nextLine();
+        if (choice == 1) {
+            searchBySlang();
+        } else if (choice == 2) {
+
+        } else if (choice == 3) {
+
+        } else if (choice == 4) {
+
+        } else if (choice == 5) {
+
+        } else if (choice == 6) {
+
+        } else if (choice == 7) {
+
+        } else if (choice == 8) {
+
+        } else if (choice == 9) {
+
+        } else if (choice == 10) {
+
+        } else {
+            WriteFile("newslang.txt");
+            System.exit(0);
+        }
+    }
+
+    public static void searchBySlang() {
+
+        System.out.print("Press a Slang word: ");
+        String key = sc.nextLine();
+        history.add(key);
+        key = key.toUpperCase();
+        if (!map.containsKey(key)) {
+            System.out.println("Not Found !");
+        } else {
+            List<String> l = map.get(key);
+            System.out.println("Definition:");
+            for (String s : l) {
+                System.out.println("- " + s);
+            }
+        }
+        pauseTerminal();
+        Menu();
+    }
+
+    public static void ReadFile(String file_name) {
+        try {
+            File f = new File(file_name);
+            FileReader fr = new FileReader(f);
+
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("`")) {
+                    List<String> tar = new ArrayList<String>();
+
+                    String[] s = line.split("`");
+                    if (s[1].contains("|")) {
+                        String[] tmp = s[1].split("\\|");
+                        for (int i = 0; i < tmp.length; i++) {
+                            tmp[i] = tmp[i].trim();
+                        }
+                        tar = Arrays.asList(tmp);
+                    } else {
+                        tar.add(s[1]);
+                    }
+                    map.put(s[0], tar);
+                }
+            }
+
+            fr.close();
+            br.close();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    public static void WriteFile(String file_name) {
+        try {
+            File f = new File(file_name);
+            FileWriter fw = new FileWriter(f);
+            for (String key : map.keySet()) {
+                fw.write(key + "`");
+                List<String> tmp = map.get(key);
+                int i = 0;
+                for (i = 0; i < tmp.size() - 1; i++) {
+                    fw.write(tmp.get(i) + "| ");
+                }
+
+                fw.write(tmp.get(i) + "\n");
+            }
+
+            fw.close();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    public static void pauseTerminal() {
+        System.out.println("-----Press Any Key To Continue-----");
+        new java.util.Scanner(System.in).nextLine();
+    }
 }
